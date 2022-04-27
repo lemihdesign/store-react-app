6962176ccf24143aa458d0a5083231dd177d0b8c
Student 249860 <lemihdesign@gmail.com>
2022-04-27 14:36:20 +0200

    Mobile styles for product items.

--- src/components/Products/ProductsItem.js
+++ src/components/Products/ProductsItem.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { cartActions } from "../../store/cart-slice";
 import Aos from "aos";
@@ -7,12 +7,17 @@ import classes from "./ProductsItem.module.css";
 import { Link } from "react-router-dom";
 
 const ProductsItem = (props) => {
+  const [isMouseOver, setIsMouseOver] = useState(false);
   const { brand, name, color, imgSmall, price, id, gender } = props;
   const dispatch = useDispatch();
 
   useEffect(() => {
     Aos.init({ duration: 1000 });
   }, []);
+
+  const onMouseOverHandler = () => {
+    setIsMouseOver(!isMouseOver);
+  };
 
   const addItemToCartHandler = () => {
     const item = {
@@ -28,31 +33,41 @@ const ProductsItem = (props) => {
   };
 
   return (
-    <div data-aos="fade-up" className={classes["product-box"]}>
-      <Link to={`${id}`}>
-        <div className={classes["product-box-label"]}>
-          <span className={classes.label}></span>
-          <div className={classes.info}>
-            <p className={classes["product-name"]}>{name}</p>
-            <p className={classes["product-brand"]}>{brand}</p>
-          </div>
+    <div
+      className={
+        isMouseOver ? classes["product-box-active"] : classes["product-box"]
+      }
+      onClick={onMouseOverHandler}
+    >
+      {/*  */}
+      <div className={classes["product-box-label"]}>
+        <span className={classes.label}></span>
+        <div className={classes.info}>
+          <p className={classes["product-name"]}>{name}</p>
+          <p className={classes["product-brand"]}>{brand}</p>
         </div>
-        <div className={classes["product-image"]}>
-          <img src={imgSmall} alt="Shoe" />
-        </div>
-        <div className={classes["product-price-box"]}>
-          <p>Price</p>
-          <p className={classes["product-price"]}>$ {price}</p>
-        </div>
-        <div className={classes["product-controls"]}>
-          <button
-            onClick={addItemToCartHandler}
-            className={classes["add-to-cart-btn"]}
-          >
-            <i className="fa-solid fa-cart-arrow-down"></i>
-          </button>
-        </div>
-      </Link>
+      </div>
+      <div className={classes["product-image"]}>
+        <img src={imgSmall} alt="Shoe" />
+      </div>
+      <div className={classes["product-price-box"]}>
+        <p className={classes["product-price"]}>${price}</p>
+      </div>
+      <div className={classes["product-controls"]}>
+        {isMouseOver && (
+          <Fragment>
+            <button onClick={addItemToCartHandler}>
+              <i className="fa-solid fa-cart-arrow-down"></i>
+            </button>
+            <Link to={`${id}`}>
+              <button>
+                <i className="fa-solid fa-magnifying-glass"></i>
+              </button>
+            </Link>
+          </Fragment>
+        )}
+      </div>
+      {/* </Link> */}
     </div>
   );
 };
